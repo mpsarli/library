c9b18f0cf24935c03b9c92aab29d35bd1896eaed
pilar.sarli <pilar.sarli@globallogic.com>
2023-06-15 09:36:50 -0300

    jsx -> tsx

--- ui/src/_components/ui/ListView.tsx
+++ ui/src/_components/ui/ListView.tsx
@@ -1,9 +1,14 @@
+import React from 'react'
 import { Datagrid, FilterLiveSearch, List, BulkDeleteButton, TopToolbar, CreateButton, DeleteButton, EditButton, FilterButton } from "react-admin"
 
+interface ListViewProps {
+    children: React.ReactNode,
+    filters?: React.ReactNode[]
+}
 const ListView = ({
     children,
     filters=[],
-}) => {
+}:ListViewProps) => {
 
     const listFilters = [
         <FilterLiveSearch source='q' alwaysOn />,
